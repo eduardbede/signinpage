87dc6dec3f2951612dfce7c9f61abe8888065eaf
eduardbede <bede.eduard@gmail.com>
2022-06-15 18:09:26 +0300

    Updated Verion of Sign In / Log In page

--- script.js
+++ script.js
@@ -7,61 +7,59 @@ const messageDiv = document.querySelectorAll(".messageDiv");
 
 
 function verifyEmailAndPass(){
-    const verify = JSON.parse(localStorage.getItem("userData"))
+    const verify = JSON.parse(localStorage.getItem("userData"));
+    if(verify === null){
+        confirmMessage.textContent = "Email does not exist!";
+        confirmMessage.style.color = "red";
+    }
      inputValue.forEach(el =>{
            if(el.value === ''){
             el.style.borderColor ="red";
            }
         });
 
-    if(email.value === ""){
-        noEmail.textContent = "This field is empty!"
-        noEmail.style.color = "red"
+    if(email.value === ''){
+        noEmail.textContent = "This field is empty!";
+        noEmail.style.color = "red";
+    }   else if (!email.value.includes("@gmail.com", "@yahoo.com", "hotmail.com", "aol.com")){
+        noEmail.textContent = "Email format is not correct!";
+        noEmail.style.color = "red";
+    }  else if (verify.findIndex(el => el.email === email.value) !== -1){
+        if(verify[verify.findIndex(el => el.email === email.value)].password === password.value){
+            confirmMessage.textContent = "Log In Succeseful!";
+            confirmMessage.style.color = "green";
+        }else if(verify.findIndex(el => el.password === password.value) === -1){
+            confirmMessage.textContent = "Password not match!";
+            confirmMessage.style.color = "red"
+        }
+     }
+        else if(verify.findIndex(el => el.email === email.value) === -1 ){
+            confirmMessage.textContent = "Email does not exist!";
+            confirmMessage.style.color = "red";
+     } 
 
-    } else if (!email.value.includes("@gmail.com", "@yahoo.com", "hotmail.com", "aol.com")){
-        noEmail.textContent = "Email is not correct!"
-    }
-    
-    if(password.value === ""){
-        noPassword.textContent = "This field is empty!"
-        noPassword.style.color = "red"
+     if(password.value === ''){
+        noPassword.textContent = "This field is empty!";
+        noPassword.style.color = "red";
     }  else if(password.value < 5){
-        noPassword.textContent = "Password too short!"
-        noPassword.style.color = "red"
+        noPassword.textContent = "Password too short!";
+        noPassword.style.color = "red";
     }
 
+}
 
     inputValue.forEach(el =>{
         el.addEventListener("click", ()=>{
             el.style.borderColor = "black";
             noEmail.textContent = "";
             noPassword.textContent = "";
-            confirmMessage.textContent = '';
+            confirmMessage.textContent = ""
         });
     });
 
-if(email.value === ''){
-
-} else if ( password.value === ''){
-
-} else if (verify.findIndex(el => el.email === email.value) !== -1){
-    if(verify[verify.findIndex(el => el.email === email.value)].password === password.value){
-        confirmMessage.textContent = "Log In Succeseful!";
-        confirmMessage.style.color = "green";
-    }else if(verify.findIndex(el => el.password === password.value) === -1){
-        confirmMessage.textContent = "Password not match!";
-        confirmMessage.style.color = "red"
-    }
- }
-    else if(verify.findIndex(el => el.email === email.value) === -1){
-        confirmMessage.textContent = "Email does not exist! Create an account!";
-        confirmMessage.style.color = "red";
- }
-}
 
 buttonCenter.addEventListener("click", ()=>{
     verifyEmailAndPass()
-    
 })
 
 
